1081860ec7d8120834dfd33c6267988bf3a83402
docfr10 <docfr10@yandex.ru>
2023-07-10 14:54:17 +0300

    Added drawer opening when the button is pressed

--- app/src/main/java/com/example/lesson_drawermenu/MainActivity.kt
+++ app/src/main/java/com/example/lesson_drawermenu/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,12 +21,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.lesson_drawermenu.ui.theme.LessonDrawerMenuTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -38,6 +42,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val navController = rememberNavController()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val coroutineScope = rememberCoroutineScope()
+
                     val currentRoute = remember { mutableStateOf("") }
 
                     navController.addOnDestinationChangedListener { _, destination, _ ->
@@ -46,47 +53,56 @@ class MainActivity : ComponentActivity() {
 
                     Scaffold(
                         content = { padding ->
-                            TopAppBar(
-                                title = { Text(text = currentRoute.value) },
-                                navigationIcon = {
-                                    IconButton(onClick = { /*TODO*/ }) {
-                                        Icon(
-                                            imageVector = Icons.Default.Menu,
-                                            contentDescription = "Drawer menu"
+                            ModalNavigationDrawer(
+                                drawerContent = {
+                                    ModalDrawerSheet {
+                                        Text("Drawer title", modifier = Modifier.padding(16.dp))
+                                        Divider()
+                                        NavigationDrawerItem(
+                                            label = { Text(text = "Item 1") },
+                                            selected = currentRoute.value == Screen.Screen1.route,
+                                            onClick = {
+                                                navController.navigate(Screen.Screen1.route)
+                                                coroutineScope.launch { drawerState.close() }
+                                            }
+                                        )
+                                        NavigationDrawerItem(
+                                            label = { Text(text = "Item 2") },
+                                            selected = currentRoute.value == Screen.Screen2.route,
+                                            onClick = {
+                                                navController.navigate(Screen.Screen2.route)
+                                                coroutineScope.launch { drawerState.close() }
+                                            }
+                                        )
+                                        NavigationDrawerItem(
+                                            label = { Text(text = "Item 3") },
+                                            selected = currentRoute.value == Screen.Screen3.route,
+                                            onClick = {
+                                                navController.navigate(Screen.Screen3.route)
+                                                coroutineScope.launch { drawerState.close() }
+                                            }
                                         )
                                     }
                                 },
-                                colors = TopAppBarDefaults.smallTopAppBarColors()
-                            )
-                            ModalNavigationDrawer(drawerContent = {
-                                ModalDrawerSheet {
-                                    Text("Drawer title", modifier = Modifier.padding(16.dp))
-                                    Divider()
-                                    NavigationDrawerItem(
-                                        label = { Text(text = "Item 1") },
-                                        selected = currentRoute.value == Screen.Screen1.route,
-                                        onClick = {
-                                            navController.navigate(Screen.Screen1.route)
-                                        }
+                                drawerState = drawerState,
+                                content = {
+                                    TopAppBar(
+                                        title = { Text(text = currentRoute.value) },
+                                        navigationIcon = {
+                                            IconButton(onClick = {
+                                                coroutineScope.launch { drawerState.open() }
+                                            }) {
+                                                Icon(
+                                                    imageVector = Icons.Default.Menu,
+                                                    contentDescription = "Drawer menu"
+                                                )
+                                            }
+                                        },
+                                        colors = TopAppBarDefaults.smallTopAppBarColors()
                                     )
-                                    NavigationDrawerItem(
-                                        label = { Text(text = "Item 2") },
-                                        selected = currentRoute.value == Screen.Screen2.route,
-                                        onClick = {
-                                            navController.navigate(Screen.Screen2.route)
-                                        }
-                                    )
-                                    NavigationDrawerItem(
-                                        label = { Text(text = "Item 3") },
-                                        selected = currentRoute.value == Screen.Screen3.route,
-                                        onClick = {
-                                            navController.navigate(Screen.Screen3.route)
-                                        }
-                                    )
+                                    MainScreen(navController = navController, padding = padding)
                                 }
-                            }) {
-                                MainScreen(navController = navController, padding = padding)
-                            }
+                            )
                         })
                 }
             }
